2d1e24507589801ccb23861e36c90b8b394c5886
makalouNana <abdoulayemakalou@esp.sn>
2022-11-08 13:50:20 +0000

    Stylisation des task

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 //RéCUPéRATION DES BALISES
 const ajoutInput = document.querySelector('.ajoutInput');
 const AjoutBouton = document.querySelector('.AjoutBouton');
-const tacheList = document.querySelector('.tacheList')
+const allTask = document.querySelector('.allTask')
 // les variables 
-
 let tache = "";
-
-
-
-
+let toDoInput
 
 //fonctions
 
@@ -21,20 +17,26 @@ const tacheRecuperation = (e) =>{
 // Fonction d'ajout d'une tache
 
 const tacheAjout = () =>{
+    //
+    const tacheList = document.createElement('div')
+    allTask.appendChild(tacheList)
+    tacheList.classList.add('tacheList')
     //ajout de l'icons complete
-    let valider = document.createElement('span')
-    valider.innerHTML= `<ion-icon name="toggle-outline"></ion-icon>`
+    const valider = document.createElement('span')
+    valider.innerHTML= `<ion-icon  name="toggle-outline"></ion-icon>`
     tacheList.appendChild(valider)
     //Ajout du conteneur de la tach
-   let toDoInput = document.createElement("input")
-   toDoInput.value = tache
-   tacheList.appendChild(toDoInput)
-   ajoutInput.value = ""
-   //Ajout de l'icone delete
-   let trash = document.createElement('span')
-   trash.innerHTML= `<ion-icon class="delete"name="trash-outline"></ion-icon>`
-   tacheList.innerHTML += `<ion-icon class="delete"name="trash-outline"></ion-icon>`
-//    tacheList.classList.add('task')
+    toDoInput = document.createElement("input")
+    toDoInput.defaultValue = tache
+    tacheList.appendChild(toDoInput)
+   toDoInput.classList.add('ajoutInput')
+      //Ajout de l'icone delete
+   const trash = document.createElement('span')
+   trash.innerHTML= `<ion-icon class="trash" name="trash-outline"></ion-icon>`
+   tacheList.innerHTML += `<ion-icon name="trash-outline"></ion-icon>`
+    //reinitialisation
+
+    ajoutInput.value = ""
 }
 
 
